cf7e00d539389050c9543fb2dc02a031544f37e8
Mikael Fernandes <mikaelluizlima.f@gmail.com>
2024-08-19 14:20:50 -0300

    add: Projeto modelagem IPhone rev2

---
+++ POO-desafio-modelagem-IPhone/src/iphone/IPhone.java
@@ -0,0 +1,30 @@
+package iphone;
+
+import iphone.components.AparelhoTelefonico;
+import iphone.components.NavegadorInternet;
+import iphone.components.ReprodutorMusical;
+
+public class IPhone {
+    private ReprodutorMusical reprodutorMusical;
+    private AparelhoTelefonico aparelhoTelefonico;
+    private NavegadorInternet navegadorInternet;
+
+    public IPhone() {
+        this.reprodutorMusical = new ReprodutorMusical();
+        this.aparelhoTelefonico = new AparelhoTelefonico();
+        this.navegadorInternet = new NavegadorInternet();
+    }
+
+    public ReprodutorMusical getReprodutorMusical() {
+        return reprodutorMusical;
+    }
+
+    public AparelhoTelefonico getAparelhoTelefonico() {
+        return aparelhoTelefonico;
+    }
+
+    public NavegadorInternet getNavegadorInternet() {
+        return navegadorInternet;
+    }
+}
+
---
+++ POO-desafio-modelagem-IPhone/src/iphone/components/NavegadorInternet.java
@@ -0,0 +1,16 @@
+package iphone.components;
+
+public class NavegadorInternet {
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+    }
+}
+
